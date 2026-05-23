2f66cf3e80acc5f4188a1723c3cdfc5557ea4b49
Your New Name <karegamavijaykumar@gmail.com>
2026-05-23 21:19:30 +0530

    Initial commit

---
+++ src/main/java/com/qbank/QuestionbankApplication.java
@@ -0,0 +1,13 @@
+package com.qbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionbankApplication.class, args);
+	}
+
+}
